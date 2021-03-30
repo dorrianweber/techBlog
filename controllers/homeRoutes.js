5cfc7478b23b0619b04f9e8e2519c44b45a3697f
Dorrian Weber <dorrianweber@gmail.com>
2021-03-29 18:24:25 -0700

    Worked on login/out functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,22 +1,52 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+// router.get('/', async (req, res) => {
+//   try {
+//     // Get all users, sorted by name
+//     const userData = await User.findAll({
+//       attributes: { exclude: ['password'] },
+//       order: [['name', 'ASC']],
+//     });
+
+//     // Serialize user data so templates can read it
+//     const users = userData.map((project) => project.get({ plain: true }));
+
+//     // Pass serialized data into Handlebars.js template
+//     res.render('homepage', { users });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/', async (req, res) => {
   try {
-    // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
+    // Get all posts, sorted by title
+    const postData = await Post.findAll({
+      order: [['title', 'ASC']],
     });
 
-    // Serialize user data so templates can read it
-    const users = userData.map((project) => project.get({ plain: true }));
+    // Serialize post data so templates can read it
+    const posts = postData.map((project) => project.get({ plain: true }));
 
     // Pass serialized data into Handlebars.js template
-    res.render('homepage', { users });
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+// Handle views
 const homeRoutes = require('./homeRoutes');
+// Handle data
+const apiRoutes = require('./api');
 
 router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
